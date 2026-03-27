444f0c5e703e5e138eccc351609acfa309e9ca0b
Mayess03 <mayes.boussaada@insat.ucar.tn>
2026-03-27 22:51:02 +0100

    added details page hotels

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,6 +6,7 @@ import ResetPassword from './pages/ResetPassword';
 import LandingPage from './pages/LandingPage';
 import HotelsPage from './pages/Hotels';
 import Contact from './pages/contact';
+import HotelDetails from './pages/HotelsDetails';
 
 function App() {
   return (
@@ -19,6 +20,7 @@ function App() {
         <Route path="/reset" element={<ResetPassword />} />
         <Route path="/hotels" element={<HotelsPage />} />
         <Route path="/contact" element={<Contact />} />
+        <Route path="/hotels/:id" element={<HotelDetails />} />
       </Routes>
     </BrowserRouter>
   );
