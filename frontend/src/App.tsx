6d073d53cc653daf65a89bb274bf201070095c85
eyamakni <makni.eya03@gmail.com>
2026-03-27 11:44:36 +0100

    changement css

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -8,6 +8,7 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
+        
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/forgot" element={<ForgotPassword />} />
